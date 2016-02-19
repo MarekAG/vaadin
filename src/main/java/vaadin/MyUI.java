384d7d3eb8b908a250e7f2a6f603de3ba3046697
MarekAG <marekwstrzala@gmail.com>
2016-02-19 09:32:54 +0100

    added simple validation

--- src/main/java/vaadin/MyUI.java
+++ src/main/java/vaadin/MyUI.java
@@ -5,6 +5,9 @@ import javax.servlet.annotation.WebServlet;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.annotations.Widgetset;
+import com.vaadin.data.Validator;
+import com.vaadin.data.validator.NullValidator;
+import com.vaadin.data.validator.StringLengthValidator;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
@@ -24,14 +27,21 @@ public class MyUI extends UI {
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         final VerticalLayout layout = new VerticalLayout();
-        
-        final TextField name = new TextField();
-        name.setCaption("Type your name here:");
 
-        Button button = new Button("Click Me");
+
+        TextField name = new TextField("Podaj swoje imię:");
+        name.addValidator(new StringLengthValidator("Zła długość", 0, 5, true));
+        Button button = new Button("Kliknij mnie");
         button.addClickListener( e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
-                    + ", it works!"));
+            name.setValidationVisible(true);
+            try {
+                name.validate();
+                layout.addComponent(new Label("Dzięki " + name.getValue()
+                        + ", to działa!"));
+            } catch (Validator.InvalidValueException err) {
+                layout.addComponent(new Label("Imię jest za długie"));
+            }
+
         });
         
         layout.addComponents(name, button);
